8a01151c8200428191df13e1e08a0554368a47d9
sherl <sherlockeria@gmail.com>
2020-02-08 21:51:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameFrag, new FragmentOne()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment mFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.homeNav:
+                        mFragment = new FragmentOne();
+                        break;
+                    case R.id.storeNav:
+                        mFragment = new FragmentTwo();
+                        break;
+                    case R.id.homeSearch:
+                        mFragment = new FragmentThree();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameFrag, mFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
